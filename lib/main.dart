20521fdc7d5db582f41cd0d76c1272c1e55c56eb
vinhbin <binepzai2004@gmail.com>
2025-02-26 13:31:18 -0500

    Update main.dart
    day and night mode toggle with sun and moon logo, color picker for the text, and updated pubspec.yaml for color picker, updated readme for install of color picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
 void main() {
-runApp(MyApp());
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-home: FadingTextAnimation(),
-);
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: FadingTextAnimation(
+        toggleTheme: toggleTheme,
+        isDarkMode: _isDarkMode,
+      ),
+    );
+  }
 }
+
 class FadingTextAnimation extends StatefulWidget {
-@override
-_FadingTextAnimationState createState() => _FadingTextAnimationState();
+  final VoidCallback toggleTheme;
+  final bool isDarkMode;
+
+  FadingTextAnimation({required this.toggleTheme, required this.isDarkMode});
+
+  @override
+  _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
+
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
-bool _isVisible = true;
-void toggleVisibility() {
-setState(() {
-_isVisible = !_isVisible;
-});
-}
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('Fading Text Animation'),
-),
-body: Center(
-child: AnimatedOpacity(
-opacity: _isVisible ? 1.0 : 0.0,
-duration: Duration(seconds: 1),
-child: Text(
-'Hello, Flutter!',
-style: TextStyle(fontSize: 24),
-),
-),
-),
-floatingActionButton: FloatingActionButton(
-onPressed: toggleVisibility,
-child: Icon(Icons.play_arrow),
-),
-);
-}
+  bool _isVisible = true;
+  Color _textColor = Colors.black;
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  void changeTextColor(Color color) {
+    setState(() {
+      _textColor = color;
+    });
+  }
+
+  void showColorPickerDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Pick a text color"),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: _textColor,
+              onColorChanged: changeTextColor,
+              showLabel: false,
+              enableAlpha: false,
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text("Select"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fading Text Animation'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.color_lens),
+            onPressed: showColorPickerDialog,
+          ),
+          IconButton(
+            icon: Icon(widget.isDarkMode ? Icons.dark_mode : Icons.light_mode),
+            onPressed: widget.toggleTheme,
+          ),
+        ],
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
+          child: Text(
+            'Hello, Flutter!',
+            style: TextStyle(fontSize: 24, color: _textColor),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
 }
